51c5f5097a03847b6e85175aac3bc2cdfa16f03e
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2021-12-14 10:36:02 +0300

    Fix note from reviewer

--- person.rb
+++ person.rb
@@ -1,19 +1,24 @@
 class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-  attr_accessor :id, :name, :age
 
   def of_age?
-    @age > 18
+    @age >= 18
   end
 
   def can_use_services?
-    @age > 18 or @parent_permission == true
+    if of_age? || @parent_permission
+      true
+    else
+      false
+    end
   end
-  private :is_of_age?
-  public :can_use_services?
+  private :of_age?
 end
--- teacher.rb
+++ teacher.rb
@@ -2,12 +2,11 @@ require './person'
 
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
   def can_use_services?
-    super()
     true
   end
 end
