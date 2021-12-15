01a5e8d10ee547f7e2570ee1144f34ef1d707e9e
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2021-12-16 01:52:49 +0300

    OOP school library: set up associations

--- student.rb
+++ student.rb
@@ -1,9 +1,12 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 
   def play_hooky
