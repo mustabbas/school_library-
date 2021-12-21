4bb3b092a24ed621d6930f1c35b1f377806b1dca
mustabbas2000@gmail.com <mustabbas2000@gmail.com>
2021-12-21 23:43:14 +0300

    Write json data into file

--- store_data.rb
+++ store_data.rb
@@ -1,11 +1,12 @@
+require 'json'
 def store_data(people, books, rentals)
   puts people
   puts books
   puts rentals
   if File.exists?("people.json")
-    File.write("people.json", people, mode: "a")
+    File.write("people.json", JSON.generate(people), mode: "a") 
   else
     # Create the file
-    File.write("people.json", people.join("\n"), mode: "w")
+    File.write("people.json", JSON.generate(people), mode: "w") 
   end
 end
